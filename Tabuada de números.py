7cbd09e4df979d9061e1e61fad06d90593020bea
Guilherme <118483441+BlearchNotDanger@users.noreply.github.com>
2023-04-19 21:06:05 -0300

    calculadoras

---
+++ Tabuada de números.py
@@ -0,0 +1,13 @@
+numero= int(input('digite um numero'))
+s= numero * 1
+s2= numero * 2
+s3= numero * 3
+s4= numero * 4
+s5= numero * 5
+s6= numero * 6
+s7= numero * 7
+s8= numero * 8
+s9= numero * 9
+s10= numero * 10
+print("A tabuada desse número é x1: {}, x2: {}, x3: {}, x4: {}, x5: {}, x6: {}, x7: {}, x8: {}, x9: {}, x10: {}."
+      .format(s, s2, s3, s4, s5, s6, s7, s8, s9, s10))
